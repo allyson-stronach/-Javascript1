6519d8ac626becb980a832c6f8f83a0fc821162e
Hackbright Student <info@hackbrightacademy.com>
2014-10-16 17:42:20 -0700

    done with ex04

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ for (var i = 0; i < 1000; i++) {
     }
 }
 console.log(sum);
-*/
+
 
 
 //EXERCISE 02 - 
@@ -56,3 +56,84 @@ console.log(fibonacci_set(1000));
 
 
 console.log(sum(even(fibonacci_set(4000000))));
+
+*/
+
+// EXERCISE 03
+/* the following code is work in progress. 
+
+var duplicateArray = [];
+
+var arr = ["katie", "true", true, 19, "gargoyles", "!", 2 + 3, "2 + 3", 19, "19", 19 === "19", 6, false, false];
+
+var o = {};
+
+for (var i = 0; i < arr.length; i++)
+{
+    if (arr[i] in o)
+    {
+        o[arr[i]] += 1;
+    }
+    else
+    {
+        o[arr[i]] = 1;
+        o.arr[i].type = typeof(arr[i]);
+    }
+}
+
+for (var prop in o)
+{
+    if (o[prop] > 1)
+    {
+        duplicateArray.push(prop);
+    }
+}
+
+console.log(duplicateArray);
+*/
+
+
+//EXERCISE 4
+
+var catObj = {
+    tiredness : 20,
+    loneliness : 3,
+    hunger: 20,
+    happiness: 15,
+    obedientness: -5000,
+    feed: function()
+    {
+        console.log("sssssslurp");
+        this.hunger = this.hunger -5;
+    },
+    cuddle: function()
+    {
+        console.log("purrrrr");
+        this.loneliness = this.loneliness -1;
+    },
+    sleepytime: function()
+    {
+        console.log("zzzzzzzzzzz");
+        this.tiredness = this.tiredness -1;
+    },
+    play: function()
+    {
+        console.log("weeeeeee");
+        this.happiness = this.happiness -5;
+    },
+    spraybottle: function()
+    {
+        console.log("hisssssss");
+        this.obedientness = this.obedientness +1;
+    },
+};
+
+function catstatus(catObj)
+{
+    for (var prop in catObj)
+    {
+        console.log(prop + ":" + catObj[prop]);
+    }
+}
+
+catstatus(catObj);
